52c37f787929246d3e02db3f104240c2dd56cdc6
leganux <leganux@hotmail.com>
2022-01-25 14:25:30 -0600

    log fixes an previews

--- public/main.js
+++ public/main.js
@@ -1,93 +1,156 @@
 const socket = io();
 
 $(document).ready(function () {
+
+    let playing = true
+
+    $('#play').click(function () {
+        playing = !playing
+        if (playing) {
+            $('#play').text('Pause')
+        } else {
+            $('#play').text('Play')
+        }
+    })
+
     socket.on("connect", function (data) {
         console.log('Cliente conectado');
     });
     socket.on("disconnect", function (data) {
         console.log('Cliente desconectado');
     });
-   /* let data = []
-    const ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'line',
-        data: data,
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: 'top',
-                },
-                title: {
-                    display: true,
-                    text: 'Chart.js Line Chart'
-                }
-            }
-        },
-    });*/
+    /* let data = []
+     const ctx = document.getElementById('myChart').getContext('2d');
+     const myChart = new Chart(ctx, {
+         type: 'line',
+         data: data,
+         options: {
+             responsive: true,
+             plugins: {
+                 legend: {
+                     position: 'top',
+                 },
+                 title: {
+                     display: true,
+                     text: 'Chart.js Line Chart'
+                 }
+             }
+         },
+     });*/
 
 
     $('#type').select2();
 
-    socket.on("logger", function (data) {
+    let tansformer = function (content) {
+        try {
+            content = JSON.parse(content)
+        } catch (e) {
+            content = content
+        }
+        if (content && typeof content == 'object') {
+            if (Array.isArray(content)) {
+                content = JSON.stringify(content)
+            } else {
+                let newContent = []
+                for (var [key, value] of Object.entries(content)) {
+                    if (typeof value == 'text') {
+                        newContent.push(value)
+                    } else {
+                        let pre = '<pre>' + JSON.stringify(value, undefined, 2) + '</pre>'
+                        newContent.push(pre)
+                    }
 
-        data = JSON.parse(data)
-        console.log(data);
+                }
+                content = newContent.join('<hr>')
+            }
+        }
+        return content
+    }
+
+    let makeIt = function (data) {
+
         let type = ''
+        let content = ''
 
+        if (data?.type == 'request') {
+            try {
+                let item = JSON.parse(data.content)
+                let kind = ''
+                switch (item?.method) {
+                    case 'POST':
+                        kind = '<span class="badge badge-primary"><h3> ' + item.method + ' </h3></span>'
+                        break;
+                    case 'PUT':
+                        kind = '<span class="badge badge-info"><h3> ' + item.method + ' </h3></span>'
+                        break;
+                    case 'GET':
+                        kind = '<span class="badge badge-success"><h3> ' + item.method + ' </h3></span>'
+                        break;
+                    case 'OPTIONS':
+                        kind = '<span class="badge badge-warning"><h3> ' + item.method + ' </h3></span>'
+                        break;
+                    case 'DELETE':
+                        kind = '<span class="badge badge-danger"><h3> ' + item.method + ' </h3></span>'
+                        break;
+                    default:
+                        kind = '<span class="badge badge-primary"><h3> ' + item.method + ' </h3></span>'
+                        break;
+                }
+
+                content = kind +
+                    '<br>' +
+                    '<label> URL: ' + item.url + '</label>' +
+                    '<br>' +
+                    '<label> STATUS:' + item.status + '</label>' +
+                    '<br>' +
+                    '<label> Body:' + '</label> ' + '<pre>' + JSON.stringify(item.body, undefined, 2) + '</pre>' +
+                    '<br>' +
+                    '<label> Query:' + '</label> ' + '<pre>' + JSON.stringify(item.query, undefined, 2) + '</pre>' +
+                    '<br>' +
+                    '<label> Params:' + '</label> ' + '<pre>' + JSON.stringify(item.params, undefined, 2) + '</pre>'
+            } catch (e) {
+                content = data.content
+                content = tansformer(content)
+            }
+        } else {
+            content = data.content
+            content = tansformer(content)
+        }
+
+        let class_ = 'table-primary'
         switch (data?.type) {
             case 'log':
                 type = '<span class="badge badge-primary">log</span>'
                 break;
             case 'info':
                 type = '<span class="badge badge-secondary">info</span>'
+                class_ = 'table-secondary'
                 break;
             case 'warn':
                 type = '<span class="badge badge-warning">warn</span>'
+                class_ = 'table-warning'
                 break;
             case 'debug':
                 type = '<span class="badge badge-info">debug</span>'
+                class_ = 'table-info'
                 break;
             case 'error':
                 type = '<span class="badge badge-danger">error</span>'
+                class_ = 'table-danger'
                 break;
             case 'request':
                 type = '<span class="badge badge-dark">request</span>'
+                class_ = 'table-light'
                 break;
             default:
                 type = '<span class="badge badge-light">other</span>'
+                class_ = 'table-dark'
                 break;
         }
 
-        let content = ''
-        if (data?.type == 'request') {
-            try {
-                let item = JSON.parse(data.content)
-                content = '<h3> ' + item.method + ' </h3>' +
-                    '<br>' +
-                    '<label> URL: ' + item.url + '</label>' +
-                    '<br>' +
-                    '<label> STATUS:' + item.status + '</label>' +
-                    '<br>' +
-                    '<label> Body:' + '</label> ' + JSON.stringify(item.body) +
-                    '<br>' +
-                    '<label> Query:' + '</label> ' + JSON.stringify(item.query) +
-                    '<br>' +
-                    '<label> Params:' + '</label> ' + JSON.stringify(item.params)
-            } catch (e) {
-                content = data.content
-            }
-        } else {
-            content = data.content
-        }
-
-        $('#preppend_here').prepend('<tr class="contexto" reference="' + data._id + '" app="' + data.app + '"source="' + data.source + '">' +
-
+        return '<tr class="contexto ' + class_ + '" reference="' + data._id + '" app="' + data.app + '"source="' + data.source + '">' +
             '<td>' +
             moment(data?.date).format() +
-            '</td>' +
-            '<td>' +
-            content +
             '</td>' +
             '<td>' +
             type +
@@ -98,7 +161,27 @@ $(document).ready(function () {
             '<td>' +
             data?.source +
             '</td>' +
-            '</tr>');
+            '<td>' +
+            content +
+            '</td>' +
+            '</tr>';
+
+
+    }
+
+    socket.on("logger", function (data) {
+
+        data = JSON.parse(data)
+
+        if (playing) {
+
+            let type_ = $('#type').val()
+            console.log(type_, data)
+            if (data?.type === type_ || type_ === '-1') {
+                $('#preppend_here').prepend(makeIt(data));
+            }
+
+        }
 
 
     });
@@ -141,73 +224,9 @@ $(document).ready(function () {
 
         $.getJSON('/log', body, function (data_) {
             for (let data of data_?.data) {
-                let type = ''
-
-                switch (data?.type) {
-                    case 'log':
-                        type = '<span class="badge badge-primary">log</span>'
-                        break;
-                    case 'info':
-                        type = '<span class="badge badge-secondary">info</span>'
-                        break;
-                    case 'warn':
-                        type = '<span class="badge badge-warning">warn</span>'
-                        break;
-                    case 'debug':
-                        type = '<span class="badge badge-info">debug</span>'
-                        break;
-                    case 'error':
-                        type = '<span class="badge badge-danger">error</span>'
-                        break;
-                    case 'request':
-                        type = '<span class="badge badge-dark">request</span>'
-                        break;
-                    default:
-                        type = '<span class="badge badge-light">other</span>'
-                        break;
-                }
-                let content = ''
-                if (data?.type == 'request') {
-                    try {
-                        let item = JSON.parse(data.content)
-                        content = '<h3> ' + item.method + ' </h3>' +
-                            '<br>' +
-                            '<label> URL: ' + item.url + '</label>' +
-                            '<br>' +
-                            '<label> STATUS:' + item.status + '</label>' +
-                            '<br>' +
-                            '<label> Body:' + '</label> ' + JSON.stringify(item.body) +
-                            '<br>' +
-                            '<label> Query:' + '</label> ' + JSON.stringify(item.query) +
-                            '<br>' +
-                            '<label> Params:' + '</label> ' + JSON.stringify(item.params)
-                    } catch (e) {
-                        content = data.content
-                    }
-                } else {
-                    content = data.content
-                }
-
-                $('#preppend_here').prepend('<tr class="contexto" reference="' + data._id + '" app="' + data.app + '"source="' + data.source + '">' +
-                    '<td>' +
-                    moment(data?.date).format() +
-                    '</td>' +
-                    '<td>' +
-                    content +
-                    '</td>' +
-                    '<td>' +
-                    type +
-                    '</td>' +
-                    '<td>' +
-                    data?.app +
-                    '</td>' +
-                    '<td>' +
-                    data?.source +
-                    '</td>' +
-                    '</tr>');
+                $('#preppend_here').prepend(makeIt(data));
             }
         })
-
 
     })
 
@@ -251,71 +270,8 @@ $(document).ready(function () {
 
         $.getJSON('/log', body, function (data_) {
             for (let data of data_?.data) {
-                let type = ''
 
-                switch (data?.type) {
-                    case 'log':
-                        type = '<span class="badge badge-primary">log</span>'
-                        break;
-                    case 'info':
-                        type = '<span class="badge badge-secondary">info</span>'
-                        break;
-                    case 'warn':
-                        type = '<span class="badge badge-warning">warn</span>'
-                        break;
-                    case 'debug':
-                        type = '<span class="badge badge-info">debug</span>'
-                        break;
-                    case 'error':
-                        type = '<span class="badge badge-danger">error</span>'
-                        break;
-                    case 'request':
-                        type = '<span class="badge badge-dark">request</span>'
-                        break;
-                    default:
-                        type = '<span class="badge badge-light">other</span>'
-                        break;
-                }
-
-                let content = ''
-                if (data?.type == 'request') {
-                    try {
-                        let item = JSON.parse(data.content)
-                        content = '<h3> ' + item.method + ' </h3>' +
-                            '<br>' +
-                            '<label> URL: ' + item.url + '</label>' +
-                            '<br>' +
-                            '<label> STATUS:' + item.status + '</label>' +
-                            '<br>' +
-                            '<label> Body:' + '</label> ' + JSON.stringify(item.body) +
-                            '<br>' +
-                            '<label> Query:' + '</label> ' + JSON.stringify(item.query) +
-                            '<br>' +
-                            '<label> Params:' + '</label> ' + JSON.stringify(item.params)
-                    } catch (e) {
-                        content = data.content
-                    }
-                } else {
-                    content = data.content
-                }
-
-                let html = '<tr class="contexto" reference="' + data._id + '" app="' + data.app + '"source="' + data.source + '">' +
-                    '<td>' +
-                    moment(data?.date).format() +
-                    '</td>' +
-                    '<td>' +
-                    content +
-                    '</td>' +
-                    '<td>' +
-                    type +
-                    '</td>' +
-                    '<td>' +
-                    data?.app +
-                    '</td>' +
-                    '<td>' +
-                    data?.source +
-                    '</td>' +
-                    '</tr>'
+                let html = makeIt(data)
 
                 if (data._id < Number(reference)) {
                     $('#preppend_here_after').append(html);
